90c62d878decf7c1b99f442c02d5827e860d93cd
Raghul PK <87524192+Raghul-PK@users.noreply.github.com>
2024-03-15 09:19:34 +0530

    Update GPS_Tracker_APIs.py

--- GPS_Tracker_APIs.py
+++ GPS_Tracker_APIs.py
@@ -2,42 +2,46 @@ from flask import Flask
 import requests, json
 app = Flask(__name__)
 
+from datetime import datetime
+
 # Get last known location of a specific vehicle
 # Identified by its GPS_id
 @app.route("/<string:gps_id>/latestLoc/", methods=['GET'])
 def getLatestLocation(gps_id):
-    gps_id = "" # For time being, as we don't have gps-id still in db
     url = "https://gps-tracker2-13e30-default-rtdb.firebaseio.com/" + gps_id + ".json?auth=zhw6Zkn3OQXjojhr8dDPn3OTJFdzhNk27c828puQ"
     req = requests.get(url)
     json_data = json.loads(req.content) # This is a dictionary
+    json_dates = json_data.keys()
+    valid_dates = [date for date in json_dates if len(date) == 6 and date.isdigit()]
+    print(valid_dates)
+
+    # Get the latest date
+    formatted_dates = [datetime.strptime(date, "%d%m%y") for date in valid_dates]
+    sorted_dates = sorted(formatted_dates)
+    sorted_dates_str = [date.strftime("%d%m%y") for date in sorted_dates]
+    print(sorted_dates_str)
 
     # Go to the last date for that particular gps_id
-    latestDate = list(json_data)[-1]
+    latestDate = list(sorted_dates_str)[-1]
     latestDate_data = json_data[latestDate]
-    print(type(latestDate_data))
+    print(latestDate_data)
 
     # Go to the last timestamp for that particular date
-    latestTime = list(latestDate_data)[-1]
+    latestTime = list(latestDate_data)[105]
     latestTime_data = latestDate_data[latestTime]
+    latestTime_data["Date"] = latestDate
+    latestTime_data["Time"] = latestTime
 
-    return [latestTime, latestTime_data]
-
-    # # Retrieve last key-value pair in dict
-    # # Originally, {'Lat': '0', 'Long': '0', 'SNR': '0'} will not be saved in final version
-    # latest_time = list(json_data)[-1]
-    # latest_loc = json_data[latest_time]
-    # print("Last tracked time : " + str(latest_time))
-    # print("Last recorded location data : " + str(latest_loc))
-    # return str(latest_time) + str(latest_loc)
+    return latestTime_data
 
 # Get journey of a specific vehicle on a specific date
 # Identified by its GPS_id
 @app.route("/<string:gps_id>/journey/<string:date>/", methods=['GET'])
 def getDateJourney(gps_id, date):
-    gps_id = ""  # For time being, as we don't have gps-id still in db
     url = "https://gps-tracker2-13e30-default-rtdb.firebaseio.com/" + gps_id + ".json?auth=zhw6Zkn3OQXjojhr8dDPn3OTJFdzhNk27c828puQ"
     req = requests.get(url)
     json_data = json.loads(req.content)  # This is a dictionary
+    print(json_data)
 
     if date in json_data:
         # Go to the specified date
